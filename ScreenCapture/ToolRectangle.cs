65300adb3eabead49c8b679f174a0fbb019c84d1
tolbKni <tolbkni@gmail.com>
2011-08-13 15:18:29 +0800

    移除冗余的 ToolObject

--- ScreenCapture/ToolRectangle.cs
+++ ScreenCapture/ToolRectangle.cs
@@ -5,13 +5,50 @@ using System.Drawing;
 namespace ScreenCapture
 {
     /// <summary>
-    /// 矩形工具
+    /// 矩形工具，创建绘图对象工具的基类
     /// </summary>
-    class ToolRectangle : ToolObject
+    class ToolRectangle : Tool
     {
+        private Cursor cursor;
+
+        #region 属性
+
+        /// <summary>
+        /// 工具的光标
+        /// </summary>
+        protected Cursor Cursor
+        {
+            get { return cursor; }
+            set { cursor = value; }
+        }
+
+        #endregion
+
+        #region 构造方法
+
         public ToolRectangle()
         {
             Cursor = Cursors.Default;
+        }
+
+        #endregion
+
+        #region 鼠标事件处理
+
+        /// <summary>
+        /// 释放鼠标左键
+        /// 改变绘制对象的大小
+        /// </summary>
+        /// <param name="regionForm"></param>
+        /// <param name="e"></param>
+        public override void OnMouseUp(RegionForm regionForm, MouseEventArgs e)
+        {
+            regionForm.DrawRectangle.Normalize();
+            // 切换成拖动模式
+            regionForm.ActiveTool = RegionForm.RegionToolType.Pointer;
+
+            regionForm.Capture = false;
+            regionForm.Refresh();
         }
 
         public override void OnMouseDown(RegionForm regionForm, MouseEventArgs e)
@@ -30,5 +67,25 @@ namespace ScreenCapture
                 regionForm.Refresh();
             }
         }
+        
+        #endregion
+
+        #region 辅助方法
+
+        /// <summary>
+        /// 左键点击绘制区时调用
+        /// 激活指定绘图对象
+        /// </summary>
+        /// <param name="regionForm"></param>
+        /// <param name="o"></param>
+        protected void ChangeObject(RegionForm regionForm, DrawRectangle o)
+        {
+            regionForm.DrawRectangle = o;
+
+            regionForm.Capture = true;
+            regionForm.Refresh();
+        }
+
+        #endregion
     }
 }
